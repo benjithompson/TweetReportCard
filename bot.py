8e5df25a58e9353598cc6d83155dd193e45ec2a6
Ben Thompson <thomp211@wwu.edu>
2017-03-01 12:55:16 -0800

    streaming, added comments, refactored

--- bot.py
+++ bot.py
@@ -1,30 +1,34 @@
 """main bot script"""
 
+
+import tweepy
+
+import twt
+from twitter import config
+from twitter import stdstream as ss
+
 """TODO:
     1. Get twitter API auth
-    2. get tweetdumps of users, specified in a config list. eg users = {'realDonaldTrump': userclass, 'Hillary': userclass, ...} defauldict
+    2. get tweetdumps of users, specified in a config list.
+        eg users = {'realDonaldTrump': userclass, 'Hillary': userclass, ...} defauldict
     3. Run textstat over all current tweetdumps to get each user's avgs and save to user object
     4. Run nltk over each user to get sentiment and save to user's object
     5. Once dumps and avgs are compiled, start stream to analyse new tweets from specified user(s)
     6. Repost tweet and stats
+    7. Handle excepts
 """
 
-import sys
-from collections import defaultdict
+def run():
+    """Main bot runner to start tweet agrigation and streaming analysis"""
+    api = twt.get_api()
+    tweeters = twt.get_user_dict(config.nameslist, api)
+    # twt.print_tweeter_names(TWEETERS)
+    # twt.load_tweets(api, TWEETERS)
+    # twt.update_tweeters_stats(TWEETERS)
+    # #twt.print_tweeter_stats(TWEETERS)
+    ss.run_stream(api, 'realDonaldTrump')
 
-from twitter import config
-import twt
-
-def run(args):
-
-    twit_dict = twt.get_user_dict(config.nameslist)
-    #twt.print_tweeter_names(twit_dict)
-
-    #iterates over twit_dict and loads each object's tweets list
-    twt.load_tweets(twit_dict)
-    twt.update_tweeters_stats(twit_dict)
-    twt.print_tweeter_stats(twit_dict)
 
 
 if __name__ == '__main__':
-    run(sys.argv)
+    run()
--- twt.py
+++ twt.py
@@ -1,44 +1,54 @@
 """holds the tweeter info"""
 
 from collections import defaultdict
-from twitter import config as c
-from textstat import textstat as ts
 from pprint import pprint
+
 import tweepy
+from textstat import textstat as ts
+
+from twitter import config as c
 
 
 def get_api():
+    """returns tweepy api object"""
     auth = tweepy.OAuthHandler(c.consumer_key, c.consumer_secret)
     auth.set_access_token(c.access_token, c.access_token_secret)
     api = tweepy.API(auth)
     return api
 
-def get_user_dict(nameslist):
+def get_user_dict(nameslist, api):
+    """returns dict of users objects specified from namelist and returned from api"""
     tweeters = defaultdict(Tweeter)
     for name in nameslist:
-        user = Tweeter.api.get_user(screen_name=name)
-        tweeters[user.name] = Tweeter(user)
+        user = api.get_user(screen_name=name)
+        tweeters[user.screen_name] = Tweeter(user)
     return tweeters
 
-def load_tweets(tweeters):
+def load_tweets(api, tweeters):
+    """Loads the last 200 tweets from the API and list of tweeters"""
     for name, tweeter in tweeters.items():
         print('loading tweets from ' + name)
-        tweeter.load_tweet_dump(200)
+        tweeter.load_tweet_dump(api, 200)
 
 def update_tweeters_stats(tweeters):
+    """Updates the stats for tweeters in list"""
     for name, tweeter in tweeters.items():
+        print(name + ' stats updated')
         tweeter.update_stats()
 
 def print_tweeter_names(tweeters):
+    """prints all tweeter names in dict"""
     for name, tweeter in tweeters.items():
         print(name + " ID: " + str(tweeter.user.id))
 
 def print_tweeter_tweets(tweeters):
+    """prints all tweets from tweeters in dict"""
     for name, tweeter in tweeters.items():
         print(name)
         tweeter.print_tweets()
 
 def print_tweeter_stats(tweeters):
+    """prints stats from all tweeters in dict"""
     for name, tweeter in tweeters.items():
         print(name)
         tweeter.print_stats()
@@ -46,19 +56,20 @@ def print_tweeter_stats(tweeters):
 
 
 class Tweeter:
-
-    api = get_api()
+    """Holds tweet text and stats as well as common user attributes for easy access"""
 
     def __init__(self, user):
         self.user = user
         self.name = user.name
         self.screen_name = user.screen_name
+        self.user_id = str(user.id)
         self.tweets = []
         self.stats = Tweeter.init_stats()
         self.tweet_cnt = 0
 
     @staticmethod
     def init_stats():
+        """initializes all stat fields to 0"""
 
         return {
             'flesch_ease': 0,
@@ -74,29 +85,9 @@ class Tweeter:
             'avgsyllables': 0
         }
 
-        #get tweet_dump (max 200 tweets)
-    def load_tweet_dump(self, count):
-        tweet_dump = Tweeter.api.user_timeline(screen_name=self.screen_name, count=count)
-        for tweet in tweet_dump:
-            self.tweets.append(tweet.text)
-        self.tweet_cnt += len(self.tweets)
-
-
-    def add_new_tweet(self, msg):
-        self.tweets.append(msg)
-        self.tweet_cnt += 1
-
-    def update_stats(self):
-        tweets_str = ''
-        for tweet in self.tweets:
-            tweets_str += tweet
-        self.stats = Tweeter.get_msg_stats(tweets_str)
-
-    def print_stats(self):
-        pprint(self.stats)
-
     @staticmethod
     def get_msg_stats(msg):
+        """creates new dict and loads it with textstat fields for given msg"""
         newstats = {}
         newstats['flesch_ease'] = ts.textstat.flesch_reading_ease(msg)
         newstats['flesch_grade'] = ts.textstat.flesch_kincaid_grade(msg)
@@ -112,7 +103,36 @@ class Tweeter:
         newstats['stdreadability'] = ts.textstat.text_standard(msg)
         return newstats
 
+    #get tweet_dump (max 200 tweets)
+    def load_tweet_dump(self, api, count):
+        """receives tweet text from api and appends to objects tweets list"""
+        tweet_dump = api.user_timeline(screen_name=self.screen_name, count=count)
+        for tweet in tweet_dump:
+            self.tweets.append(tweet.text)
+        self.tweet_cnt += len(self.tweets)
+
+
+    def add_new_tweet(self, msg):
+        """appends msg to objects tweets list"""
+        self.tweets.append(msg)
+        self.tweet_cnt += 1
+
+    def update_stats(self):
+        """trivial way to update stats by concatinating all tweets from tweets list and
+           reavaluating stats on entire str"""
+        tweets_str = ''
+        for tweet in self.tweets:
+            tweets_str += tweet
+        self.stats = Tweeter.get_msg_stats(tweets_str)
+
+    def print_stats(self):
+        """prints stats dict using pprint"""
+        pprint(self.stats)
+
+
     def update_stat(self, newmsg):
+        """Incomplete: Optimized way to update numerical stats by updating via single msg
+           weight"""
         new_val = ts.textstat.flesch_reading_ease(newmsg)
         mean = self.stats['flesch_ease']
         num_tweets = len(self.tweets)
@@ -122,6 +142,6 @@ class Tweeter:
         #TODO:update all stats
 
     def print_tweets(self):
+        """prints all tweets from self tweets list"""
         for tweet in self.tweets:
             print(tweet)
-
