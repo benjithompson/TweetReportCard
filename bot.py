ad56c42d6591ca5b10854f890da596175909737f
Ben Thompson <thomp211@wwu.edu>
2017-03-02 01:07:21 -0800

    minimizing functions to run tests

--- bot.py
+++ bot.py
@@ -1,31 +1,31 @@
 """main bot script"""
 
 import twt
-from twitter import config
 from twitter import stdstream as ss
+from twitter import config
+
 
 """TODO:
-    1. Get twitter API auth
-    2. get tweetdumps of users, specified in a config list.
-        eg users = {'realDonaldTrump': userclass, 'Hillary': userclass, ...} defauldict
-    3. Run textstat over all current tweetdumps to get each user's avgs and save to user object
-    4. Run nltk over each user to get sentiment and save to user's object
-    5. Once dumps and avgs are compiled, start stream to analyse new tweets from specified user(s)
-    6. Repost tweet and stats
-    7. Handle excepts
+X. Get twitter API auth - DONE
+X. get tweetdumps of users, specified in a config list. -DONE
+   eg users = {'realDonaldTrump': userclass, 'Hillary': userclass, ...} defauldict -DONE
+    3. Run textstat over all current tweetdumps to get each user's avgs and save to user object -PASSING
+    4. Run nltk over each user to get sentiment and save to user's object - NOT STARTED
+    5. Once dumps and avgs are compiled, start stream to analyse new tweets from specified user(s) -IN PROGRESS
+    6. Repost tweet and stats - NOT STARTED
+    7. Handle excepts - IN PROGRESS
+    8. sys.argv command line args - NOT STARTED
 """
 
 def run():
     """Main bot runner to start tweet agrigation and streaming analysis"""
 
-    api = twt.get_api()
-    tweeters = twt.get_user_dict(config.nameslist, api)
-    # twt.print_tweeter_names(tweeters)
-    # twt.load_tweets(api, tweeters)
+    tweeters = twt.get_peer_dict(config.nameslist)
+    twt.print_tweeter_names(tweeters)
+    # twt.load_tweets(tweeters)
     # twt.update_tweeters_stats(tweeters)
-    # #twt.print_tweeter_stats(tweeters)
-    ss.run_stream(api, 'realDonaldTrump')
 
+    twt.listener(tweeters, 5*60)
 
 if __name__ == '__main__':
     run()
