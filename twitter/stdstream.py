9e3fed31e249a2ba8d4695fce9d7359b4752ad70
Ben Thompson <thomp211@wwu.edu>
2017-03-02 01:09:56 -0800

    separated stream functions. comments.

--- twitter/stdstream.py
+++ twitter/stdstream.py
@@ -1,26 +1,43 @@
 """Streaming Class and methods"""
 
 import tweepy
+import twt
 
 class StreamListener(tweepy.StreamListener):
     """Listens for stream updates and responds"""
 
     cnt = 0
+
     def on_status(self, tweet):
+        """Handles new status updates from stream"""
+
         print(str(StreamListener.cnt) + ':\n ' +tweet.text)
         StreamListener.cnt += 1
 
     def on_error(self, status_code):
+        """Handles errors on stream"""
         if status_code == 420:
             #disconnect, rate limiting
             return False
         else:
             print(status_code)
 
-def run_stream(api, name):
+def print_stream(user):
+    """prints stream for given name to stdout"""
+
+    stream = get_stream()
+    try:
+        stream.filter(follow=[user.user_id], async=False)
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt')
+        exit(1)
+
+def get_stream():
     """starts a tweepy stream listening to name in tweeters dict"""
-    
-    user = api.get_user(screen_name=name)
+
     listener = StreamListener()
-    stream = tweepy.Stream(auth=api.auth, listener=listener)
-    stream.filter(follow=[str(user.id)])
+    try:
+        stream = tweepy.Stream(auth=twt.API.auth, listener=listener)
+    except:
+        print('Exception: Unable to create stream')
+    return stream
