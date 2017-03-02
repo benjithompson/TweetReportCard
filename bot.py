23a60aa3a7e03b9565724563085f48c46892ff90
Ben Thompson <thomp211@wwu.edu>
2017-03-02 14:04:36 -0800

    using pickle to load prev tweets

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 """main bot script"""
 
-import twt
+import twt, os
+from utils import io
 from twitter import stdstream as ss
 from twitter import config
 
@@ -15,17 +16,30 @@ X. get tweetdumps of users, specified in a config list. -DONE
     6. Repost tweet and stats - NOT STARTED
     7. Handle excepts - IN PROGRESS
     8. sys.argv command line args - NOT STARTED
+    9. pickle - IN PROGRESS
 """
 
 def run():
     """Main bot runner to start tweet agrigation and streaming analysis"""
 
-    tweeters = twt.get_peer_dict(config.nameslist)
-    twt.print_tweeter_names(tweeters)
-    twt.load_tweets(tweeters, 0)
-    twt.update_tweeters_stats(tweeters)
+    #open pickle if exists
+    filename = input('Load pickle file: ')
+    path = io.get_path(filename)
+    print('loading pickle from {0}'.format(path))
+    tweeters = io.load_pickle(path)
+    if tweeters is not None:
+        twt.update_tweets(tweeters)
+        twt.print_tweeter_tweets(tweeters)
+    else:
+        print('Downloading tweet dump for:')
+        tweeters = twt.get_peer_dict(config.nameslist)
+        twt.print_tweeter_names(tweeters)
+        twt.load_tweets(tweeters, 0)
+        twt.update_tweeters_stats(tweeters)
 
     twt.listener(tweeters, 5*60)
+    filename = input('save pickle file: ')
+    io.save_data_to_file(tweeters, filename)
 
 if __name__ == '__main__':
     run()
---
+++ utils/io.py
@@ -0,0 +1,70 @@
+"""Handles file read/write, pickling and user input"""
+
+import os
+import sys
+import datetime
+import pickle
+
+def get_path(filename, folder='', ext=''):
+    """Utility function to create file paths"""
+    path = os.getcwd() + folder
+    filename = os.path.join(path, filename + ext)
+    return filename
+
+def save_data_to_file(data, filename):
+    """iterates through all posts and comments and writes them to specified file"""
+
+    try:
+        if __verify_write(filename):
+            __write_data(data, filename)
+        else:
+            print('aborting file write...')
+    except OSError as err:
+        print(err.strerror)
+
+def __verify_write(filename):
+    """Return true if user wants to overwrite existing file"""
+
+    if os.path.exists(filename):
+        ans = input(filename + ' already exists. Overwrite? (y/n): ')
+        return ans == 'y' or ans == 'Y'
+    else:
+        ans = input('Do you really want to write to ' + filename + '(y/n)?: ')
+        return ans == 'y' or ans == 'Y'
+
+def __write_data(data, filename):
+    try:
+        pickle.dump(data, open(filename, "wb"))
+        print('pickle successfully written')
+    except pickle.PickleError as perr:
+        print(perr)
+
+def read_from_file(filename):
+    """read file into string"""
+
+    text = ''
+
+    with open(filename, 'r') as f:
+        for line in f.readline():
+            text += line
+
+    return text
+
+def load_pickle(path):
+    """opens object from pickle file at path"""
+
+    data = None
+
+    try:
+        with open(path, 'rb') as pfile:
+            data = pickle.load(pfile)
+            print('pickle loaded!')
+
+    except pickle.UnpicklingError as perr:
+        print(perr)
+
+    except FileNotFoundError as fnfe:
+        print(fnfe)
+
+    return data
+        
